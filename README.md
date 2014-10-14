cce755e85c4ac3c08de1a86551ee1c5934c56c90
Joseph Bozarth <bozarjp@gmail.com>
2014-10-14 00:54:43 -0500

    Initial commit of all materials

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
 SimpleGallery
 =============
+Author: Joseph Bozarth,
+Date Created: 10-13-2014
 
 A clean, easy to use, straight forward jQuery gallery
+
+
+Overview
+------------------
+When I was doing some work on my personal website I thought it would be nice to have some gallery type functionality. So I went looking but I could not find anything that was not overly complex for what I needed. I just wanted to display some pictures to go with my build posts, not sell anything grab attention with flashy graphics.
+
+You can see a live demo on my site gallery: http://bozarthprime.github.io/gallery.html
+
+
+Implementation
+------------------
+Include jQuery, jQuery UI, SimpleGallery.js, and SimpleGallery.css
+
+
+Usage
+------------------
+The idea is that you will have a class that will denote your galleries (I will use '.gallery' in my example) and you just call SimpleGallery() on that class on document ready. All objects with that class will become galleries. You can do this with multiple classes with different settings if you wish.
+
+Example:
+
+<script>
+	$(document).ready(function() {
+		$('.gallery').Gallery();
+	});
+</script>
+
+<div class='gallery'>
+	<img src='img1.jpg' comment='Custom faces for the meeples in Galaxy Trucker.' />
+	<img src='img2.jpg' />
+	<img src='img3.jpg' comment='My Chalkboard Christmas Tree' />
+	<img src='img4.png' />
+</div>
+
+The comment attribute on the image is how you leave annotations for images.
+
+Options
+------------------
+height: The maximum height a gallery can be, if it would be over this it will scroll (default: 500)
+width: The width of the gallery (default: 100)
+heightIsPercent: If the height is px or percent (default: false)
+widthIsPercent: If the width is px or percent (default: true)
+nextImage: Path to an image for the next image button (default: text reading 'Next')
+prevImage: Path to an image for the previous image button (default: text reading 'Prev.')
+closeImage: Path to an image for the close button (default: capital 'X' in consolas font)
---
+++ scripts/SimpleGallery.js
@@ -0,0 +1,164 @@
+(function ($) {
+    $.fn.SimpleGallery = function (options) {
+        var settings = $.extend({
+            height: 500,
+            width: 100,
+			heightIsPercent: false,
+			widthIsPercent: true,
+			nextImage: "",
+			prevImage: "",
+			closeImage: ""
+        }, options);
+
+        var element = $(this);
+		var curImage;
+		var curGallery;
+		
+		$('body').append("<div id='shadow'></div>");
+		$('body').append("<div id='lightbox'></div>");
+		$('#lightbox').append("<div id='img-cont'></div>");
+		$('#lightbox').append("<div id='img-note'></div>");
+		
+		if (settings.closeImage != "") {
+			$('#lightbox').append("<div id='img-close'><img src='" + settings.closeImage + "' /></div>");
+		} else {
+			$('#lightbox').append("<div id='img-close'>X</div>");
+		}
+		
+		if (settings.nextImage != "") {
+			$('#lightbox').append("<div id='img-next'><img src='" + settings.nextImage + "' /></div>");
+		} else {
+			$('#lightbox').append("<div id='img-next'>Next</div>");
+		}
+		
+		if (settings.prevImage != "") {
+			$('#lightbox').append("<div id='img-prev'><img src='" + settings.prevImage + "' /></div>");
+		} else {
+			$('#lightbox').append("<div id='img-prev'>Prev.</div>");
+		}
+		
+		
+		element.css('text-align', 'center');
+		element.css('vertical-align', 'middle');
+		element.css('overflow', 'auto');
+		
+		if (settings.heightIsPercent) {
+			element.css('max-height', settings.height + '%');
+		} else {
+			element.css('max-height', settings.height + 'px');
+		}
+		
+		if (settings.widthIsPercent) {
+			element.css('width', settings.width + '%');
+		} else {
+			element.css('width', settings.width + 'px');
+		}
+		
+		var galleryImages = element.children('img');
+		
+		galleryImages.css('max-height', '100%');
+		galleryImages.css('max-width', '200px');
+		galleryImages.css('display', 'inline-block');
+		
+		function close() {
+			$('#lightbox').hide(0,function() {
+				$('#img-note').hide();
+				$('#shadow').slideUp(function(){
+					$('#img-cont').html('');
+					$('#img-note').html('');
+				});
+			});
+			
+			curImage = null;
+		}
+		
+		function nextImage() {
+			if (curImage < curGallery.length - 1) {
+				curImage++;
+			} else {
+				curImage = 0;
+			}
+			
+			setImage();
+		}
+		
+		function prevImage() {
+			if (curImage > 0) {
+				curImage--;
+			} else {
+				curImage = curGallery.length - 1;
+			}
+			
+			setImage();
+		}
+		
+		function setImage() {
+			var image = $(curGallery[curImage]).clone();
+
+			if(image.attr('comment')) {
+				$('#img-note').html(image.attr('comment'));
+				$('#img-note').show();
+			} else {
+				$('#img-note').hide();
+			}
+
+			image.css('max-height', '100%');
+			image.css('max-width', '100%');
+			image.css('display', 'block');
+			
+			$('#img-cont').html(image);			
+		}
+		
+		galleryImages.click(function() {
+			curGallery = $(this).parent().children('img');
+			var image = $(this).clone();
+			
+			for(var i = 0; i < curGallery.length; i++) {
+				if ($(curGallery[i]).attr('src') == $(this).attr('src')) {
+					curImage = i;
+				}
+			}
+			
+			$('#shadow').slideDown(function(){
+				if(image.attr('comment')) {
+					$('#img-note').html(image.attr('comment'));
+					$('#img-note').show();
+				} else {
+					$('#img-note').hide();
+				}
+			});
+
+			image.css('max-height', '100%');
+			image.css('max-width', '100%');
+			image.css('display', 'block');
+			
+			image.hide();
+			$('#img-cont').html(image);
+
+			$('#lightbox').show(function() {
+				image.slideDown(200);
+			});
+		});
+		
+		$('#img-cont').click(function() {
+			nextImage();
+		});
+		
+		$('#img-next').click(function() {
+			nextImage();
+		});
+		
+		$('#img-prev').click(function() {
+			prevImage();
+		});
+		
+		$('#shadow').click(function() {
+			close();
+		});
+		
+		$('#img-close').click(function() {
+			close();
+		});
+		
+    }
+})(jQuery); 
